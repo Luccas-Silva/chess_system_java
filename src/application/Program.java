3e07e5222673ce20a4d9abc87ee713339537257e
Luccas Silva <lluccas.anjoss@gmail.com>
2022-02-11 11:41:33 -0300

    project created

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Hello world!");
+		
+		sc.close();
+
+	}
+
+}
