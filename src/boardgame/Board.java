b1797f9464a9fce9db2c21c2dae27feaf3f42ce0
Luccas Silva <lluccas.anjoss@gmail.com>
2022-03-01 15:20:52 -0300

    Check logic

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,6 +28,7 @@ public class Board {
 		}
 		return pieces[row][column];
 	}
+	
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
@@ -42,6 +43,8 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	
 	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
@@ -58,9 +61,11 @@ public class Board {
 	public boolean positionExists (int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns; 
 	}
+	
 	public boolean positionExists (Position position) {
 		return positionExists(position.getRow(),position.getColumn());
 	}
+	
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
 		throw new BoardException("Position not on the board");
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -15,9 +15,11 @@ public abstract class Piece {
 	}
 	
 	public abstract boolean[][] possibleMoves();
+	
 	public boolean possibleMoves(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
+	
 	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
 		for (int i=0; i<mat.length; i++) {
